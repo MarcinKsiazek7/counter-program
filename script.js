94fe9015ac7c13bd1ed3c9553ddc01d1ca2711ec
Marcin <ksiazek.marcin7@gmail.com>
2025-02-25 15:28:56 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const addBtn = document.getElementById("addBtn");
+const countLabel = document.getElementById("countLabel");
+
+
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+
+addBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
